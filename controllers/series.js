28602c3703fcad1d16bbe6292f84b5acdfa99c35
Amanda Martins <mandimartins23@gmail.com>
2019-07-22 12:57:55 -0300

    create  new serie functionality

--- controllers/series.js
+++ controllers/series.js
@@ -1,22 +1,22 @@
-const Serie = require('../models/serie')
-
-const index = (req,res)=>{
+const index = ({Serie}, req,res)=>{
     Serie.find({},(err,docs)=>{
         res.render('series/index',{
             series: docs
         })
     })
 }
-const nova = (req,res)=>{
-    const serie = new Serie({
-        name:'The 100',
-        status:'watched'
-    })
-    serie.save(()=> console.log('saved'))
-    res.render('series/nova',)
+const novaProcess = ({Serie}, req,res)=>{
+    const serie = new Serie(req.body)
+    serie.save(()=> res.redirect('/series'))
+    
+}
+
+const novaForm = (req,res)=>{
+    res.render('series/nova')
 }
 
 module.exports = {
     index,
-    nova
+    novaProcess,
+    novaForm
 }
