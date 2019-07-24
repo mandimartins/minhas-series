9f3fcc3251f71d36974fd8633d8250c933fbdcd5
Amanda Martins <mandimartins23@gmail.com>
2019-07-24 10:21:44 -0300

    edit and delete working

--- controllers/series.js
+++ controllers/series.js
@@ -1,22 +1,54 @@
+const labels = [
+    {id:'to-watch', name:'Para Assistir'},
+    {id:'watching', name:'Assistindo'},
+    {id:'watched', name:'Assistido'}
+ ]
 const index = ({Serie}, req,res)=>{
     Serie.find({},(err,docs)=>{
         res.render('series/index',{
-            series: docs
+            series: docs,
+            labels
         })
     })
 }
 const novaProcess = ({Serie}, req,res)=>{
     const serie = new Serie(req.body)
     serie.save(()=> res.redirect('/series'))
-    
 }
-
 const novaForm = (req,res)=>{
     res.render('series/nova')
 }
 
+const excluir = ({Serie},req, res)=>{
+    Serie.remove({
+        _id:req.params.id
+    },(err)=>{
+        res.redirect('/series')
+    })
+}
+const editarProcess = ({Serie}, req,res)=>{
+    Serie.findOne({
+        _id:req.params.id
+    },(err, serie)=>{
+        serie.name = req.body.name
+        serie.status = req.body.status
+        serie.save()
+        res.redirect('/series')
+    })
+}
+const editarForm = ({Serie},req,res)=>{
+
+    Serie.findOne({_id: req.params.id},(err,serie)=>{   
+        res.render('series/editar',{ serie, labels })
+    })
+}
+
+
 module.exports = {
     index,
     novaProcess,
-    novaForm
+    novaForm,
+    excluir,
+    editarForm,
+    editarProcess
 }
