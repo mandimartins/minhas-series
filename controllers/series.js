84bf268936e5aee12278c67c668e2557ef4a6398
Amanda Martins <mandimartins23@gmail.com>
2019-07-24 16:36:12 -0300

    comment section added

--- controllers/series.js
+++ controllers/series.js
@@ -12,11 +12,17 @@ const index = async ({Serie}, req,res)=>{
 }
 const novaProcess = async({Serie}, req,res)=>{
     const serie = new Serie(req.body)
-    await serie.save()
-    res.redirect('/series')
+    try{
+        await serie.save()
+        res.redirect('/series')
+    }catch(e){
+        res.render('series/nova',{
+            errors:Object.keys(e.errors)
+        })
+    }   
 }
 const novaForm = (req,res)=>{
-    res.render('series/nova')
+    res.render('series/nova',{errors:[]})
 }
 
 const excluir = async({Serie},req, res)=>{
@@ -31,12 +37,24 @@ const editarProcess = async ({Serie}, req,res)=>{
     })
         serie.name = req.body.name
         serie.status = req.body.status
-        await serie.save()
-        res.redirect('/series')
+        try{
+            await serie.save()
+            res.redirect('/series')
+        }catch(e){
+            res.render('series/editar',{ serie, labels, errors:Object.keys(e.errors) })
+        }
 }
 const editarForm = async({Serie},req,res)=>{
     const serie = await Serie.findOne({_id: req.params.id})
-    res.render('series/editar',{ serie, labels })
+    res.render('series/editar',{ serie, labels, errors:[] })
+}
+const info = async({Serie},req,res)=>{
+    const serie = await Serie.findOne({_id:req.params.id})
+    res.render('series/info',{serie})
+}
+const addComentario = async({Serie},req,res)=>{
+    await Serie.updateOne({_id:req.params.id},{$push:{comments:req.body.comentario}})
+    res.redirect('/series/info/'+req.params.id)
 }
 module.exports = {
     index,
@@ -44,5 +62,7 @@ module.exports = {
     novaForm,
     excluir,
     editarForm,
-    editarProcess
+    editarProcess,
+    info,
+    addComentario
 }
